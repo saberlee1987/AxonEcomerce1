3f891c47ce1089b6c7f5d6c39125075f95a26a59
saberlee66 <saberazizi66@yahoo.com>
2020-10-15 19:19:41 +0330

    add @EnableEurekaServer

--- ecom-registry/src/main/java/com/saber/ecom/registry/EcomRegistryApplication.java
+++ ecom-registry/src/main/java/com/saber/ecom/registry/EcomRegistryApplication.java
@@ -2,8 +2,10 @@ package com.saber.ecom.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EcomRegistryApplication {
 
 	public static void main(String[] args) {
