5f7a44e5968dda778e754da509263a5963e1c072
saberlee66 <saberazizi66@yahoo.com>
2020-10-15 16:29:07 +0330

    add config server

---
+++ ecom-config-server/src/main/java/com/saber/ecom/config_server/EcomConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.saber.ecom.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomConfigServerApplication.class, args);
+	}
+
+}
---
+++ ecom-config-server/src/test/java/com/saber/ecom/config_server/EcomConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saber.ecom.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
